28946f0562dc43b1ecaa6099b8903bc1162adf83
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-11 22:42:16 +0330

    Create AppSettings.cs

---
+++ Configuration/AppSettings.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Cactus.Blade.Configuration
+{
+    /// <summary>
+    /// Defines an indexer property that retrieves settings from the "AppSettings" section from
+    /// <see cref="Config.Root"/>.
+    /// </summary>
+    public sealed class AppSettings
+    {
+        private AppSettings() { }
+
+        internal static AppSettings Instance { get; } = new AppSettings();
+
+        /// <summary>
+        /// Gets the setting associated with the <paramref name="key"/> parameter.
+        /// </summary>
+        /// <param name="key">The key of the setting to look up.</param>
+        /// <returns>The setting associated with the <paramref name="key"/> parameter.</returns>
+        /// <exception cref="KeyNotFoundException">
+        /// If the given key is not found in the "AppSettings" section of <see cref="Config.Root"/>.
+        /// </exception>
+        public string this[string key] => Config.Root[$"AppSettings:{key}"] ?? throw GetKeyNotFoundException(key);
+
+        private static Exception GetKeyNotFoundException(string key)
+        {
+            return new KeyNotFoundException(
+                $"Unable to locate {nameof(Config.AppSettings)} key '{key}' in {typeof(Config).FullName}.{nameof(Config.Root)}.");
+        }
+    }
+}
