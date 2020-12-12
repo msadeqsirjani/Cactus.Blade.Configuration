86619f1ab05b0e2f747f6434d231b8c0eb904027
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-12 11:54:56 +0330

    Create Qux.cs

---
+++ Configuration.Sample.EnvironmentVariableConfig/Qux.cs
@@ -0,0 +1,9 @@
+﻿namespace Configuration.Sample.EnvironmentVariableConfig
+{
+    public class Qux
+    {
+        public Garply Garply { get; set; }
+
+        public Foo[] Foos { get; set; }
+    }
+}
