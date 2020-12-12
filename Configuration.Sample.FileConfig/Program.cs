b2e21f7fdb8c8720ba997268297fb88db3596a05
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-12 11:55:06 +0330

    Create Program.cs

---
+++ Configuration.Sample.FileConfig/Program.cs
@@ -0,0 +1,39 @@
+﻿using Cactus.Blade.Configuration;
+using Cactus.Blade.Configuration.ObjectFactory;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System;
+
+namespace Configuration.Sample
+{
+    internal class Program
+    {
+        internal static void Main(string[] args)
+        {
+            Console.WriteLine("Configuration Manager Example Harness");
+
+            try
+            {
+                var key1 = Config.AppSettings["Key1"];
+                var defaultConnectionString = Config.Root.GetConnectionString("Default");
+
+                var fooConfigSection = Config.Root.GetSection("Foo");
+                var foo = fooConfigSection.Create<Foo>();
+
+                var foo2ConfigSection = Config.Root.GetSection("Foo");
+                var foo2 = foo2ConfigSection.Create<Foo>();
+
+                Console.WriteLine($"key1: {key1}");
+                Console.WriteLine($"defaultConnectionString: {defaultConnectionString}");
+                Console.WriteLine($"foo: {JsonConvert.SerializeObject(foo)}");
+                Console.WriteLine($"foo is same instance as foo2: {foo.ReferenceEquals(foo2)}");
+
+                var notFound = Config.AppSettings["notFound"];
+            }
+            catch (Exception)
+            {
+                // ignored
+            }
+        }
+    }
+}
