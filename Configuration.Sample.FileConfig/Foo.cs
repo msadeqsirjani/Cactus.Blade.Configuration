ecc001783c8956cdce44c870b5a83e692398de9f
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-12 11:55:05 +0330

    Create Foo.cs

---
+++ Configuration.Sample.FileConfig/Foo.cs
@@ -0,0 +1,9 @@
+﻿namespace Configuration.Sample
+{
+    internal class Foo
+    {
+        public int Bar { get; set; }
+        public string Baz { get; set; }
+        public bool Qux { get; set; }
+    }
+}
